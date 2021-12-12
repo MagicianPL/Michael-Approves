535f40becde0363e18bb45dcc35c924abdf61906
Michał <sukowskimichal@gmail.com>
2021-12-12 11:45:15 +0100

    bg images on every item

--- src/components/RestaurantItem.tsx
+++ src/components/RestaurantItem.tsx
@@ -2,20 +2,24 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { useCurvedText } from '../helpers/useCurvedText';
 
-const Wrapper = styled.div`
+interface Props {
+    bg: string,
+}
+
+const Wrapper = styled.div<Props>`
     width: 100%;
     max-width: 310px;
-    aspect-ratio: 1 / 1;
-    border: 1px solid red;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    margin-bottom: 25px;
 
     h1 {
         letter-spacing: 3px;
         font-weight: bold;
         font-size: 40px;
+        height: 70px;
     }
 
     .circle {
@@ -26,18 +30,34 @@ const Wrapper = styled.div`
         border-radius: 50%;
         position: relative;
         bottom: 21px;
+        cursor: pointer;
+
+        &::before {
+            content: "";
+            position: absolute;
+            inset: 0;
+            border-radius: 50%;
+            background-image: url("${props => props.bg}");
+            background-size: cover;
+            background-position: center;
+            opacity: 0.6;
+            transition: all 0.4s;
+        }
+
+        &:hover::before {
+            opacity: 0.3;
+        }
     }
 `;
 
 const RestaurantItem: React.FC<any> = ({data}) => {
-    console.log(data);
     const curvedText = useRef(null);
 
     useCurvedText(curvedText);
 
     return (
-        <Wrapper>
-            <h1 ref={curvedText}>Angel's Wings</h1>
+        <Wrapper bg={data.bgImage}>
+            <h1 ref={curvedText}>{data.name}</h1>
             <div className="circle"></div>
         </Wrapper>
     )
