9f6ad74b89a195c03b2d9dfd715b6f0d3c38e584
Michał <sukowskimichal@gmail.com>
2021-12-13 21:37:19 +0100

    restaurantdetails styled header

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -7,15 +7,21 @@ import star from '../assets/star.png';
 const StyledHeader = styled.header`
     width: 100%;
     display: flex;
+    align-items: center;
     color: ${({theme}) => theme.colors.primary};
 
     h1 {
         font-size: 60px;
         font-weight: bold;
+        padding-left: 100px;
+        margin-right: 80px;
     }
 
     p {
         font-size: 55px;
+        display: flex;
+        align-items: center;
+        gap: 15px;
     }
 
     img {
