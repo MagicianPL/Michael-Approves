8503f94745311dee439630509ec74933b6557ea4
Michał <sukowskimichal@gmail.com>
2021-12-13 21:25:06 +0100

    RestaurantDetails component

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -1,10 +1,31 @@
 import React, { useContext, useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { RestaurantsContext } from '../contexts/RestaurantsContext';
+import star from '../assets/star.png';
 
+const StyledHeader = styled.header`
+    width: 100%;
+    display: flex;
+    color: ${({theme}) => theme.colors.primary};
+
+    h1 {
+        font-size: 60px;
+        font-weight: bold;
+    }
+
+    p {
+        font-size: 55px;
+    }
+
+    img {
+        width: 55px;
+    }
+`;
 
 const RestaurantDetails: React.FC<any> = (props) => {
     const {id} = useParams();
+    //choosed restaurant object
     const object = useContext(RestaurantsContext).filter((obj: any) => obj._id === id)[0];
     const[restaurant, setRestaurant] = useState<any | null>(null);
 
@@ -22,7 +43,13 @@ const RestaurantDetails: React.FC<any> = (props) => {
 
    return (
        <>
-        {restaurant ? <h1>{restaurant.name}</h1> : null}
+        {restaurant ? 
+        <StyledHeader>
+            <h1>{restaurant.name}</h1>
+            <p>{`${restaurant.rating}/5`} <img src={star} alt="star" /></p>
+        </StyledHeader>
+        : null}
+        {object === undefined ? <p>Cannot find restaurant</p> : null}
        </>
    );
 };
