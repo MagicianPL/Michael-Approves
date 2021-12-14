4252a2c32e807ad9147bbe0391599c8aac2d23ca
Michał <sukowskimichal@gmail.com>
2021-12-14 20:11:44 +0100

    photos

--- src/components/RestaurantDetails/Photos.tsx
+++ src/components/RestaurantDetails/Photos.tsx
@@ -3,31 +3,42 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
     width: 100%;
-    border: 1px solid red;
     min-height: 300px;
     display: flex;
     justify-content: space-around;
+    align-items: center;
     transform: translateY(40px);
     opacity: 0;
     animation: appearing 0.7s forwards;
+    background: ${({theme}) => theme.colors.tertiary};
+    border-radius: 8px;
+    padding: 20px;
 
     @keyframes appearing {
         100% {
             opacity: 1;
             transform: translateY(0);
         }
-    }
+`;
 
-    img {
-        width: 100%;
-        max-width: 400px;
-        image-position: center;
-        display: block;
-        transition: all 0.5s;
+const StyledImageContainer = styled.div`
+    width: 100%;
+    max-width: 400px;
+    height: 300px;
+    overflow: hidden;
+    transition: all 0.7s;
 
         &:hover {
             transform: scale(1.2);
         }
+
+    img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+        border-radius: 5px;
     }
 `;
 
@@ -39,7 +50,10 @@ const Photos: React.FC<Props> = ({photos}) => {
     console.log(photos);
     return(
         <StyledWrapper>
-            {photos.map((photo: string) => <img key={photo} src={photo} alt="meal" />)}
+            {photos.map((photo: string) => 
+            <StyledImageContainer>
+                <img key={photo} src={photo} alt="meal" />
+            </StyledImageContainer>)}
         </StyledWrapper>
     );
 };
