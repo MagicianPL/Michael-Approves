bfe6375451295b9e945931c96ea880743bfdb698
Michał <sukowskimichal@gmail.com>
2021-12-15 19:35:50 +0100

    address

--- src/components/RestaurantDetails/RestaurantDetails.tsx
+++ src/components/RestaurantDetails/RestaurantDetails.tsx
@@ -7,6 +7,7 @@ import StyledFooter from './StyledFooter';
 import star from '../..//assets/star.png';
 import StyledButton from '../StyledButton';
 import Photos from './Photos';
+import StyledAddress from './StyledAddress';
 
 const RestaurantDetails: React.FC<any> = (props) => {
     const {id} = useParams();
@@ -41,6 +42,9 @@ const RestaurantDetails: React.FC<any> = (props) => {
                 <p>{restaurant.type}</p>
                 <p className="desc">{restaurant.description}</p>
                <Photos photos={restaurant.images.urls} />
+               <StyledAddress>
+                   <p>{restaurant.contact.address}</p>
+               </StyledAddress>
             </StyledMain>
             <StyledFooter>
                 <StyledButton as="a" href="/" marginZero>HOME</StyledButton>
--- src/components/RestaurantDetails/StyledAddress.tsx
+++ src/components/RestaurantDetails/StyledAddress.tsx
@@ -4,3 +4,5 @@ const StyledAddress = styled.section`
     width: 100%;
     border: 1px solid red;
 `;
+
+export default StyledAddress;
